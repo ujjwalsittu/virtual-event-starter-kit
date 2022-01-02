522e77c494573130ba2c0b0ada390b6458726505
Ujjwal Kumar Sittu <ujju.sittu@gmail.com>
2022-01-02 14:16:40 +0530

    git edit

--- lib/constants.ts
+++ lib/constants.ts
@@ -14,19 +14,19 @@
  * limitations under the License.
  */
 
-export const SITE_URL = 'https://demo.vercel.events';
+export const SITE_URL = 'https://hackonaut.tech';
 export const SITE_ORIGIN = process.env.NEXT_PUBLIC_SITE_ORIGIN || new URL(SITE_URL).origin;
-export const TWITTER_USER_NAME = 'vercel';
-export const BRAND_NAME = 'ACME';
-export const SITE_NAME_MULTILINE = ['ACME', 'Conf'];
-export const SITE_NAME = 'ACME Conf';
+export const TWITTER_USER_NAME = 'ujjwalsittu';
+export const BRAND_NAME = 'HackONaut';
+export const SITE_NAME_MULTILINE = ['HackONaut', 'Fest'];
+export const SITE_NAME = 'HackONaut';
 export const META_DESCRIPTION =
-  'This is an open source demo that Next.js developers can clone, deploy, and fully customize for events. Created through collaboration of marketers, designers, and developers at Vercel.';
+  'With the enthusiam to create a week full of coding and technology environment, we are annoucing HackONaut Fest to bring the Tech Entrepreneur amoung our youth..';
 export const SITE_DESCRIPTION =
-  'An interactive online experience by the community, free for everyone.';
-export const DATE = 'October 27, 2020';
-export const SHORT_DATE = 'Oct 27 - 9:00am PST';
-export const FULL_DATE = 'Oct 27th 9am Pacific Time (GMT-7)';
+  'An interactive experience by the community, free for everyone.';
+export const DATE = 'June 17, 2021';
+export const SHORT_DATE = 'June 17 - 9:00am IST';
+export const FULL_DATE = 'June 17th 9am ASIA/Kolkata (GMT +5:30)';
 export const TWEET_TEXT = META_DESCRIPTION;
 export const COOKIE = 'user-id';
 
@@ -35,27 +35,27 @@ export const COOKIE = 'user-id';
 export const LEGAL_URL = process.env.NEXT_PUBLIC_PRIVACY_POLICY_URL;
 export const COPYRIGHT_HOLDER = process.env.NEXT_PUBLIC_COPYRIGHT_HOLDER;
 
-export const CODE_OF_CONDUCT =
-  'https://www.notion.so/vercel/Code-of-Conduct-Example-7ddd8d0e9c354bb597a0faed87310a78';
-export const REPO = 'https://github.com/vercel/virtual-event-starter-kit';
+export const CODE_OF_CONDUCT = 'https://hackonaut.tech';
+  // 'https://www.notion.so/vercel/Code-of-Conduct-Example-7ddd8d0e9c354bb597a0faed87310a78';
+export const REPO = 'https://github.com/ujjwalsittu/virtual-event-starter-kit';
 export const SAMPLE_TICKET_NUMBER = 1234;
 export const NAVIGATION = [
   {
     name: 'Stage A',
     route: '/stage/a'
   },
-  {
-    name: 'Stage C',
-    route: '/stage/c'
-  },
-  {
-    name: 'Stage M',
-    route: '/stage/m'
-  },
-  {
-    name: 'Stage E',
-    route: '/stage/e'
-  },
+  // {
+  //   name: 'Stage C',
+  //   route: '/stage/c'
+  // },
+  // {
+  //   name: 'Stage M',
+  //   route: '/stage/m'
+  // },
+  // {
+  //   name: 'Stage E',
+  //   route: '/stage/e'
+  // },
   {
     name: 'Schedule',
     route: '/schedule'
